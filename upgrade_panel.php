dcd58ad5efd4b070c6f6498b97c4c5e6d4e7e022
Joldis Mhai Alexandru <jmihaialexandru@gmail.com>
2013-09-01 17:25:44 +0300

    Updated Gold Cart Registration api.

--- upgrade_panel.php
+++ upgrade_panel.php
@@ -11,13 +11,24 @@ function wpsc_activate_gold_module() {
 		if ( isset( $_POST['activation_key'] ) ){
 			update_option( 'activation_key', $_POST['activation_key'] );
 		}
+		
+		//Action to perform
+		if (isset($_POST['submit_values'])) {
+			//register api
+			$action = 'register';
+		} 
+		if (isset($_POST['reset_values'])) {
+			//reset api
+			$action = 'reset';
+			update_option( 'activation_key', '');
+		}
 
 		$useragent = 'WP e-Commerce plugin';
 		$activation_name = urlencode( $_POST['activation_name'] );
 		$activation_key = urlencode( $_POST['activation_key'] );
 		$siteurl = urlencode( get_option( 'siteurl' ) );
 		$request = '';
-		$http_request  = "GET /wp-goldcart-api/api_register.php?name=$activation_name&key=$activation_key&url=$siteurl HTTP/1.0\r\n";
+		$http_request  = "GET /wp-goldcart-api/api_register.php?name=$activation_name&key=$activation_key&url=$siteurl&action=$action HTTP/1.0\r\n";
 		$http_request .= "Host: getshopped.org\r\n";
 		$http_request .= "Content-Type: application/x-www-form-urlencoded; charset=".get_option( 'blog_charset' )."\r\n";
 		$http_request .= "Content-Length: ".strlen( $request )."\r\n";
@@ -26,10 +37,24 @@ function wpsc_activate_gold_module() {
 		$http_request .= $request;
 		$response = '';
 
-
-//		CHANGED FOR NEW ACTIVATION 2/25/13 Edward per Gary
-//		if ( false != ( $fs = @fsockopen( 'instinct.co.nz',80,$errno,$errstr,10 ) ) ) {
-		if ( false != ( $fs = @fsockopen( 'getshopped.org',80,$errno,$errstr,10 ) ) ) {
+		
+		//Added cURL activation option. 31.08.2013
+		//Try cURL first
+		if ( function_exists( 'curl_init' ) ) {
+			$ch = curl_init();
+			curl_setopt_array($ch, array(
+				CURLOPT_RETURNTRANSFER => 1,
+				//http://www.localhost.com:8080/getshopped/
+				//getshopped.org/wp-goldcart-api/
+				CURLOPT_URL => "http://www.localhost.com:8080/getshopped/api_register.php?name=$activation_name&key=$activation_key&url=$siteurl&action=$action",
+				CURLOPT_USERAGENT => $useragent,
+				CURLOPT_SSL_VERIFYPEER => false,
+				CURLOPT_SSL_VERIFYHOST => false
+			));			
+			$returned_value = curl_exec($ch);
+			curl_close($ch);
+		} else {
+			$fs = @fsockopen( 'getshopped.org',80,$errno,$errstr,10 );
 			fwrite( $fs,$http_request );
 			while ( !feof( $fs ) ){
 				$response .= fgets( $fs,1160 ); // One TCP-IP packet
@@ -37,47 +62,50 @@ function wpsc_activate_gold_module() {
 			fclose( $fs );
 			$response = explode( "\r\n\r\n",$response,2 );
 			$returned_value = (int)trim( $response[1] );
-			//$returned_value = 1;
-			if ( $returned_value == 1 ) {
-				if( get_option( 'activation_state' ) != 'true' ) {
-					update_option( 'activation_state','true' );
-					gold_shpcrt_install();
-				}
-				?>
-					<div class="updated" style="min-width:45%; max-width:463px;">
-						<p>
-							<?php _e( 'Thanks! The Gold Cart upgrade has been activated.', 'wpsc_gold_cart' ); ?><br />
-							<?php printf( __( 'New options have been added to %s, and your payment gateway list has been extended.', 'wpsc_gold_cart' ), sprintf( '<a href="options-general.php?page=wpsc-settings&tab=presentation">%s</a>', __( 'Settings -> Presentation', 'wpsc_gold_cart' ) ) ); ?>
-						</p>
-					</div>
-				<?php
-			} else {
-				update_option( 'activation_state',"false" );
-				echo '<div class="error"><p>'.__( 'Sorry, the API key was incorrect.' ,'wpsc_gold_cart' ).'</p></div>';
+		}
+		//$returned_value = 1;
+		if ( $returned_value === 'MQ==' ) {
+			if( get_option( 'activation_state' ) != 'true' ) {
+				update_option( 'activation_state','true' );
+				gold_shpcrt_install();
 			}
+			?>
+				<div class="updated" style="min-width:45%; max-width:463px;">
+					<p>
+						<?php _e( 'Thanks! The Gold Cart upgrade has been activated.', 'wpsc_gold_cart' ); ?><br />
+						<?php printf( __( 'New options have been added to %s, and your payment gateway list has been extended.', 'wpsc_gold_cart' ), sprintf( '<a href="options-general.php?page=wpsc-settings&tab=presentation">%s</a>', __( 'Settings -> Presentation', 'wpsc_gold_cart' ) ) ); ?>
+					</p>
+				</div>
+			<?php
+		} else if  ( $returned_value === 'Mg==' ){
+			//Reset API
+			update_option( 'activation_state',"false" );
+			echo '<div class="error"><p>Gold Cart upgrade has been deactivated!</p></div>';
+		} else {
+			echo '<div class="error"><p>'.$returned_value.'</p></div>';
 		}
 	}
 }
 add_action( 'wpsc_gold_module_activation','wpsc_activate_gold_module' );
 
 /**
- * Activation Form
+ * Registration Form
  */
 function wpsc_gold_activation_form() {
 	?>
 	<div class="postbox">
-		<h3 class="hndle"><?php _e( 'Gold Cart Activation', 'wpsc_gold_cart' );?></h3>
+		<h3 class="hndle"><?php _e( 'Gold Cart Registration', 'wpsc_gold_cart' );?></h3>
 		<?php if ( get_option( 'activation_state' ) == 'true' ) { ?>
 		<p>
+			<?php _e( 'Gold Cart is currently registered.', 'wpsc_gold_cart' ); ?>
 			<img align="middle" src="<?php echo WPSC_CORE_IMAGES_URL; ?>/tick.png" alt="" />
-			<?php _e( 'The gold cart is currently activated.', 'wpsc_gold_cart' ); ?>
 		</p>
 		<?php	} else { ?>
 		<p>
 			<img align="middle" src="<?php echo WPSC_CORE_IMAGES_URL; ?>/cross.png" alt="" />
-			<?php _e( 'The gold cart is currently deactivated.', 'wpsc_gold_cart' ); ?>
+			<?php _e( 'Gold Cart is currently not registered.', 'wpsc_gold_cart' ); ?>
 		</p>
-		<?php } // End activation state ?>
+		<?php } // End Registration state ?>
 		<p>
 			<label for="activation_name"><?php _e( 'Name ', 'wpsc_gold_cart' ); ?>:</label>
 			<input type="text" id="activation_name" name="activation_name" size="48" value="<?php echo get_option( 'activation_name' ); ?>" class="text" />
@@ -89,8 +117,18 @@ function wpsc_gold_activation_form() {
 		<p>
 			<input type="hidden" value="true" name="activate_gold_module" />
 			<input type="submit" class="button-primary" value="<?php _e( 'Submit', 'wpsc_gold_cart' ); ?>" name="submit_values" />
-			<input type="submit" class="button" value="<?php _e( 'Reset API Key', 'wpsc_gold_cart' ); ?>" name="reset_values" onclick="document.getElementById('activation_key').value=''" />
+			<input type="submit" class="button" value="<?php _e( 'Reset API Key', 'wpsc_gold_cart' ); ?>" name="reset_values" />
 		</p>
+		<?php if ( ! function_exists( 'curl_init' ) ) : ?>
+			<p style='color: red; font-size:8pt; line-height:10pt;'>
+				<?php _e( 'In order to register your API information your server requires cURL which is not installed on this server. We will attempt to use fsockopen as an alternate method to register Gold Cart. ', 'wpsc' ); ?>
+			</p>
+		<?php endif; ?>
+		<?php if ( ! function_exists( 'fsockopen' ) && ! function_exists( 'curl_init' )) : ?>
+			<p style='color: red; font-size:8pt; line-height:10pt;'>
+				<?php _e( 'In order to register your API information your server requires cURL or the fscockopen extension which are not installed on this server, you may need to contact your web hosting provider to get them set up. ', 'wpsc' ); ?>
+			</p>
+		<?php endif; ?>	
 		<?php
 		if ( get_option( 'activation_state' ) == "true" ) {
 			echo '<p><strong>'.__( 'Click <a href="http://docs.getshopped.org/category/extending-your-store/premium-plugins/gold-cart/" target="_blank">here</a> to learn how to use each Gold Cart feature.', 'wpsc_gold_cart' ).'</strong></p>';
