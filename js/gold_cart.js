9f3d562f4b9f6bb9d662c6a0df58ee1d1adf1e48
Joldis Mihai Alexandru <jmihaialexandru@gmail.com>
2015-01-17 02:12:09 +0200

    Fixes #8

--- js/gold_cart.js
+++ js/gold_cart.js
@@ -31,7 +31,7 @@
 		});
 	}
 	
-	$('.wpsc_live_search_embed').live('keyup', function(){
+	$('.wpsc_live_search_embed').on('keyup', function(){
 		var t = $(this),
 			str = $.trim(t.val()),
 			list = $('.' + WPSC_GoldCart.productListClass);
@@ -92,7 +92,7 @@
 		}
 	});
 	
-	$('#wpsc-main-search select').live('change', function(){
+	$('#wpsc-main-search select').on('change', function(){
 		var t = $(this), qs;
 		if (t.val() !== '') {
 			location.search = $.query.SET(t.attr('name'), t.val());
@@ -102,7 +102,7 @@
 	var widgetCache = {},
 		widgetSearch = false;
 	
-	$('.wpsc_live_search').live('keyup', function(){
+	$('.wpsc_live_search').on('keyup', function(){
 		var t = $(this),
 			str = $.trim(t.val());
 		
